3590fe1898f0b058dff553a402fa9f22ecc932f7
Alexander <alexanderkoltygin@gmail.com>
2019-11-16 14:03:40 +0600

    Add symfony/dotend. Change APPPATH. Add ROOTPATH.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
-define('APPPATH', realpath('../app/'));
-require_once APPPATH . '/../vendor/autoload.php';
+define('ROOTPATH', realpath(__DIR__ . '/../'));
+define('APPPATH', realpath(ROOTPATH . '/app/'));
+require_once ROOTPATH . '/vendor/autoload.php';
 use Core\Route;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(ROOTPATH . '/.env');
+
 new Route();
