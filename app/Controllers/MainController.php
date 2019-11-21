92a2a67c54811666fabf60a4b3de1a8753fcfbf0
Alexander <alexanderkoltygin@gmail.com>
2019-11-21 22:02:03 +0600

    Add faker. Add link for create and delete user.

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -2,16 +2,25 @@
 
 use App\Core\Controller;
 use App\Models\User;
+use Faker\Factory;
 
 class MainController extends Controller
 {
+    private $user;
+    private $faker;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->user = new User();
+        $this->faker = Factory::create('ru_RU');
+    }
+
     public function index()
     {
-        $user = new User();
-
         $this->view->render('home', [
             'title' => 'Главная страница',
-            'users' => $user->all(),
+            'users' => $this->user->all(true),
         ]);
     }
 
@@ -20,5 +29,21 @@ class MainController extends Controller
         $this->view->render('hello', [
             'title' => 'Hello, world!',
         ]);
+    }
+
+    public function addUser()
+    {
+        $gender = rand(0, 1) ? 'male' : 'female';
+        $this->user->insert([
+            'name' => $this->faker->firstName($gender),
+            'surname' => $this->faker->lastName($gender),
+        ]);
+        header('Location: /');
+    }
+
+    public function deleteUser($id)
+    {
+        $this->user->delete($id);
+        header('Location: /');
     }
 }
