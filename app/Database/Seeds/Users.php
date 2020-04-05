b118b31cd25a7c688053220736b9214986ff8885
Geriler <alexanderkoltygin@gmail.com>
2020-04-05 14:19:55 +0600

    Change seeds

--- app/Database/Seeds/Users.php
+++ app/Database/Seeds/Users.php
@@ -2,21 +2,28 @@
 
 use App\Core\Database\Seeder;
 use App\Models\User;
+use Faker\Factory;
+use Faker\Generator;
 
 class Users extends Seeder
 {
     private User $userModel;
+    private Generator $faker;
 
     public function __construct()
     {
         $this->userModel = new User();
+        $this->faker = Factory::create('ru_RU');
     }
 
     function run()
     {
-        $this->userModel->insert([
-            'name' => 'John',
-            'surname' => 'Smith',
-        ]);
+        for ($i = 0; $i < 50; $i++) {
+            $gender = rand(0, 1) ? 'male' : 'female';
+            $this->userModel->insert([
+                'name' => $this->faker->firstName($gender),
+                'surname' => $this->faker->lastName($gender),
+            ]);
+        }
     }
 }
