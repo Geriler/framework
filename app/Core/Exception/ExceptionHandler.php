a4170bfcfef598a1aa7bff82356b1fa3e4549b93
Geriler <alexanderkoltygin@gmail.com>
2020-04-05 12:16:52 +0600

    Rename controller amd model. Change ExceptionHandler. Add properties type.

--- app/Core/Exception/ExceptionHandler.php
+++ app/Core/Exception/ExceptionHandler.php
@@ -7,22 +7,17 @@ class ExceptionHandler
 {
     static function handle(Exception $exception)
     {
-        $view = new View();
-        switch (get_class($exception)) {
-            case PageNotFountException::class:
-                $view->render('errors/404', [
-                    'title' => 'Страница не найдена'
-                ], 404);
-                break;
-            case DatabaseException::class:
-            default:
-                $view->render('errors/exception', [
-                    'title' => 'Ошибка',
-                    'code' => $exception->getCode(),
-                    'message' => $exception->getMessage(),
-                    'trace' => $exception->getTraceAsString(),
-                ], 500);
-                break;
+        $exception_class = get_class($exception);
+        if (method_exists($exception_class, 'renderError')) {
+            $exception_class::renderError();
+        } else {
+            $view = new View();
+            $view->render('errors/exception', [
+                'title' => 'Ошибка',
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+                'trace' => $exception->getTraceAsString(),
+            ], 500);
         }
     }
 }
--- app/Core/Exception/PageNotFountException.php
+++ app/Core/Exception/PageNotFountException.php
@@ -1,8 +1,15 @@
 <?php namespace App\Core\Exception;
 
+use App\Core\View;
 use Exception;
 
 class PageNotFountException extends Exception
 {
-
+    static function renderError()
+    {
+        $view = new View();
+        $view->render('errors/404', [
+            'title' => 'Страница не найдена'
+        ], 404);
+    }
 }
