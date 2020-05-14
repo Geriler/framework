8df4f5ed70302d8a9819ca293ed09fc3bf3bc938
Geriler <alexanderkoltygin@gmail.com>
2020-05-14 21:16:02 +0600

    Add request

--- app/routes.php
+++ app/routes.php
@@ -7,5 +7,6 @@ Route::setDefaultAction('index');
 
 Route::add('/hello', 'MainController@hello', 'hello');
 Route::add('/user/create', 'MainController@addUser', 'addUser');
+Route::add('/user/create_user', 'MainController@createUser', 'createUser');
 Route::add('/user/update/(\d+)', 'MainController@updateUser', 'updateUser');
 Route::add('/user/delete/(\d+)', 'MainController@deleteUser', 'deleteUser');
